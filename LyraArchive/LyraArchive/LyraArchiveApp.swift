da8c6960aa616f63519ee916f979f42b1e1e38c4
하동훈 <harukax99@naver.com>
2025-09-17 19:07:41 +0900

    chore(project): initialize SwiftUI app and base folder structure
    - Created initial SwiftUI project
      (초기 SwiftUI 프로젝트 생성)
    
    - Added base folder structure: Models, Views, ViewModels, Services, Resources, Utils
      (기본 폴더 구조 추가: Models, Views, ViewModels, Services, Resources, Utils)
    
    - Prepared Assets.xcassets with default icons and colors
      (Assets.xcassets에 기본 아이콘과 색상 세팅)
    
    - Set up LyraArchiveApp.swift entry point
      (LyraArchiveApp.swift 엔트리 포인트 설정)

---
+++ LyraArchive/LyraArchive/LyraArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyraArchiveApp.swift
+//  LyraArchive
+//
+//  Created by 하동훈 on 17/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct LyraArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            // 
+        }
+    }
+}
